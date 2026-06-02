1e5ed35e24aee00db58ee58c108874dd23b33bc9
szczuras7 <szczuras7@proton.me>
2026-06-02 04:14:14 +0200

    fix import

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Product } from "../types";
 import { getProducts } from "../api/api";
 import { ProductsContext } from "./ProductsContext";
